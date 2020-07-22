7561c37bbd49258ff58fd8dec2717baee8fdff52
asijachirag <41501067+asijachirag@users.noreply.github.com>
2020-07-22 22:20:27 +0530

    Updated app.js
    I use regex and string parsing methods for returning news items

--- app.js
+++ app.js
@@ -2,142 +2,72 @@ const express = require("express");
 const app = express();
 
 const request = require("request");
-const cheerio = require("cheerio");
 
 const port = process.env.PORT || 8080;
 
 const url = "https://time.com/";
-app.get("/getTimeNews", (req, res) => {
-  request(url, function (error, response, html) {
-    var $ = cheerio.load(html);
-    const result = [];
-    $(".lead .secondary .slide .title").each(function (i, elem) {
-      const $elem = $(elem);
-      const $link = $elem.find("a");
-      const $title = $elem.find("a");
-      const news = {
-        title: $title.text(),
-        link: "https://time.com/"+$link.attr("href"),
-      };
-      result.push(news);
-    });
-    $(" .breaking li").each(function (i, elem) {
-      const $elem = $(elem);
-      var classes = $elem.attr("class");
-      if (!classes.includes("show-credit")) {
-        const $link = $elem.find(".item .slide .content .title a");
-        const $title = $elem.find(".item .slide .content .title a");
-        const news = {
-          title: $title.text(),
-          link: "https://time.com/"+$link.attr("href"),
-        };
-        result.push(news);
-      }
-    });
-    var count=0;
-    $(" .coronavirus li").each(function (i, elem) {
-        const $elem = $(elem);
-        var classes = $elem.attr("class");
-        if (!classes.includes("show-credit")&&count==0) {
-          const $link = $elem.find(".item .slide .content .title a");
-          const $title = $elem.find(".item .slide .content .title a");
-          const news = {
-            title: $title.text(),
-            link: "https://time.com/"+$link.attr("href"),
-          };
-          result.push(news);
-          count=1;
-        }
-      });
-      
-    res.json({ news: result });
-  });
-});
+const result=[];
+const result2=[];
+function find(x)
+{
+  var t=x.split(">");
+  var ref=t[1].split("=");
+  var heading=t[2].split("<");
+  const news  = {
+    title: heading[0],
+    link: "https://time.com/"+ref[1],
+  };
+  result.push(news);
+}
+ app.get("/getTimeNews", (req, res) => {
+    request(url, function (error, response, html){
 
+  var string1=html.replace(/['"]+/g, '');
+  var string2=string1.replace(/(\r\n|\n|\r|\t)/gm,"");
+  var final=string2.replace(/\s\s+/g, ' ');
+  var temp=final.split("</header>");
+  var a=temp[1].match(/<section(.)class(.*?)lead(.*?)>(.*?)<\/section>/g);
+  var a1=a[0].match(/div(.)class[=]secondary(.)>(.*?)<\/article>(.)<\/div>/g);
+  var a2=a1[0].match(/<h\d(.*?)title>(.*?)<\/h\d>/g);
+ // console.log(a2);
+  a2.forEach(find);
 
-app.get("/getallTimeNews", (req, res) => {
-    request(url, function (error, response, html) {
-      var $ = cheerio.load(html);
-      const result = [];
-      $(".lead .secondary .slide .title").each(function (i, elem) {
-        const $elem = $(elem);
-        const $link = $elem.find("a");
-        const $title = $elem.find("a");
-        const news = {
-          title: $title.text(),
-          link: "https://time.com/"+$link.attr("href"),
-        };
-        result.push(news);
-      });
-      $(" .breaking li").each(function (i, elem) {
-        const $elem = $(elem);
-        var classes = $elem.attr("class");
-        if (!classes.includes("show-credit")) {
-          const $link = $elem.find(".item .slide .content .title a");
-          const $title = $elem.find(".item .slide .content .title a");
-          const news = {
-            title: $title.text(),
-            link: "https://time.com/"+$link.attr("href"),
-          };
-          result.push(news);
-        }
-      });
-      $(" .coronavirus li").each(function (i, elem) {
-          const $elem = $(elem);
-          var classes = $elem.attr("class");
-          if (!classes.includes("show-credit")) {
-            const $link = $elem.find(".item .slide .content .title a");
-            const $title = $elem.find(".item .slide .content .title a");
-            const news = {
-              title: $title.text(),
-              link: "https://time.com/"+$link.attr("href"),
-            };
-            result.push(news);
-          }
-        });
-        $(" .election li").each(function (i, elem) {
-          const $elem = $(elem);
-          var classes = $elem.attr("class");
-          if (!classes.includes("show-credit")) {
-            const $link = $elem.find(".item .slide .content .title a");
-            const $title = $elem.find(".item .slide .content .title a");
-            const news = {
-              title: $title.text(),
-              link: "https://time.com/"+$link.attr("href"),
-            };
-            result.push(news);
-          }
-        });
-        $(" .finance li").each(function (i, elem) {
-          const $elem = $(elem);
-          var classes = $elem.attr("class");
-          if (!classes.includes("show-credit")) {
-            const $link = $elem.find(".item .slide .content .title a");
-            const $title = $elem.find(".item .slide .content .title a");
-            const news = {
-              title: $title.text(),
-              link: "https://time.com/"+$link.attr("href"),
-            };
-            result.push(news);
-          }
-        });
-        $(" .health li").each(function (i, elem) {
-          const $elem = $(elem);
-          var classes = $elem.attr("class");
-          if (!classes.includes("show-credit")) {
-            const $link = $elem.find(".item .slide .content .title a");
-            const $title = $elem.find(".item .slide .content .title a");
-            const news = {
-              title: $title.text(),
-              link: "https://time.com/"+$link.attr("href"),
-            };
-            result.push(news);
-          }
-        });
+  var b=temp[1].match(/<section(.)class=homepage-module(.)spotlight(.*?)>(.*?)<\/section>/g);
+  var b1=b[0].match(/<ul(.)class(.*?)breaking(.)point>(.*?)<\/ul>/g);
+  var b2=b1[0].match(/<li((?!show-credit).)*class((?!show-credit).)*item(.)show(.*?)>(.*?)<\/li>/g);
+  var b3=b2[0].match(/<h\d(.*?)title>(.*?)<\/h\d>/g);
+ // console.log(b3);
+  b3.forEach(find);
+
+  var c=temp[1].match(/<section(.)class=homepage-module(.)topic(.*?)>(.*?)<\/section>/g);
+  var c1=c[0].match(/<ul(.)class(.*?)coronavirus(.)in-depth>(.*?)<\/ul>/g);
+  var c2=c1[0].match(/<li((?!show-credit).)*class((?!show-credit).)*item(.)show(.*?)>(.*?)<\/li>/g);
+  var c3=c2[0].match(/<h\d(.*?)title>(.*?)<\/h\d>/g);
+  find(c3[0]);
+
       res.json({ news: result });
     });
-  });
-
+ });
+function find2(x)
+{
+    var t=x.split(">");
+    var ref=t[1].split("=");
+    var heading=t[2].split("<");
+    const news  = {
+        title: heading[0],
+        link: "https://time.com/"+ref[1],
+    };
+    result2.push(news);
+}
+  
+app.get("/getallTimeNews", (req, res) => {
+    request(url, function (error, response, html) {
+        var value=html.match(/<h(.)(.*[\s+\"\']title[\s+\"\'].*)>(.*?)<\/h\1>/g);
+        // console.log(value);
+        value.forEach(find2);
+       res.json({ news: result2 });
+    });
+ });
 
 
 app.listen(port, () => {
