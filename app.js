147dd8efc08569d31fa2709e188f7b2c0587570c
asijachirag <41501067+asijachirag@users.noreply.github.com>
2020-07-23 20:56:46 +0530

    Update getLatestStories
    Append the code for returning Latest Stories with route /getLatestStories

--- app.js
+++ app.js
@@ -6,69 +6,80 @@ const request = require("request");
 const port = process.env.PORT || 8080;
 
 const url = "https://time.com/";
-let result=[];
-let result2=[];
-function find(x)
+
+
+function find(items)
 {
-  let t=x.split(">");
-  let ref=t[1].split("=");
-  let heading=t[2].split("<");
-  const news  = {
-    title: heading[0],
-    link: "https://time.com/"+ref[1],
-  };
-  result.push(news);
-  return;
-}
- app.get("/getTimeNews", (req, res) => {
-    request(url, function (error, response, html){
-        result=[];
-  let string1=html.replace(/["]+/g, '');
-  let string2=string1.replace(/(\r\n|\n|\r|\t)/gm,"");
-  let final=string2.replace(/\s\s+/g, ' ');
-  let split_by_header=final.split("</header>");
-  let split_by_lead=split_by_header[1].match(/<section(.)class(.*?)lead(.*?)>(.*?)<\/section>/g);
-  let split_by_secondary=split_by_lead[0].match(/div(.)class[=]secondary(.)>(.*?)<\/article>(.)<\/div>/g);
-  let split_by_title1=split_by_secondary[0].match(/<h\d(.*?)title>(.*?)<\/h\d>/g);
- split_by_title1.forEach(find);
-
-  let split_by_spotlight=split_by_header[1].match(/<section(.)class=homepage-module(.)spotlight(.*?)>(.*?)<\/section>/g);
-  let split_by_breaking_point=split_by_spotlight[0].match(/<ul(.)class(.*?)breaking(.)point>(.*?)<\/ul>/g);
-  let split_by_li=split_by_breaking_point[0].match(/<li((?!show-credit).)*class((?!show-credit).)*item(.)show(.*?)>(.*?)<\/li>/g);
-  let split_by_title2=split_by_li[0].match(/<h\d(.*?)title>(.*?)<\/h\d>/g);
- split_by_title2.forEach(find);
-
-  let split_by_topic=split_by_header[1].match(/<section(.)class=homepage-module(.)topic(.*?)>(.*?)<\/section>/g);
-  let split_by_coronavirus=split_by_topic[0].match(/<ul(.)class(.*?)coronavirus(.)in-depth>(.*?)<\/ul>/g);
-  let split_by_item=split_by_coronavirus[0].match(/<li((?!show-credit).)*class((?!show-credit).)*item(.)show(.*?)>(.*?)<\/li>/g);
-  let split_by_title3=split_by_item[0].match(/<h\d(.*?)title>(.*?)<\/h\d>/g);
-  find(split_by_title3[0]);
-
-      res.json({ news: result });
-    });
- });
-function find2(x)
-{
-    let t=x.split(">");
+  let result=[];
+  for(item of items)
+  { 
+    let t=item.split(">");
     let ref=t[1].split("=");
     let heading=t[2].split("<");
     const news  = {
-        title: heading[0],
-        link: "https://time.com/"+ref[1],
+      title: heading[0],
+      link: "https://time.com/"+ref[1],
     };
-    result2.push(news);
-    return;
+    result.push(news);
+  }
+  return result;
 }
+
+function splitting(html)
+{
+    let string1=html.replace(/["]+/g, '');
+    let string2=string1.replace(/(\r\n|\n|\r|\t)/gm,"");
+    let final=string2.replace(/\s\s+/g, ' ');
+    let split_by_header=final.split("</header>");
+    return split_by_header;
+}
+
+ 
+app.get("/getTimeNews", (req, res) => {
+    request(url, function (error, response, html){
+
+    let split_header=splitting(html);
+    let split_by_lead=split_header[1].match(/<section[^<>]*lead?[^<>]*>(.*?)<\/section>/g);
+    let split_by_secondary=split_by_lead[0].match(/div[^<>]*secondary(.)>(.*?)<\/article>(.)<\/div>/g);
+    let split_by_title1=split_by_secondary[0].match(/<h(.)[^<>]*title?[^<>]*>(.*?)<\/h\1>/g);
+    let arr1=find(split_by_title1);
+
+    let split_by_spotlight=split_header[1].match(/<section[^<>]*spotlight[^<>]*>(.*?)<\/section>/g);
+    let split_by_breaking_point=split_by_spotlight[0].match(/<ul[^<>]*breaking(.)point?[^<>]*>(.*?)<\/ul>/g);
+    let split_by_li=split_by_breaking_point[0].match(/<li((?!show-credit).)*item(.)show?[^<>]*>(.*?)<\/li>/g);
+    let split_by_title2=split_by_li[0].match(/<h(.)[^<>]*title?[^<>]*>(.*?)<\/h\1>/g);
+    let arr2=find(split_by_title2);
+
+    let split_by_topic=split_header[1].match(/<section[^<>]*topic[^<>]*>(.*?)<\/section>/g);
+    let split_by_coronavirus=split_by_topic[0].match(/<ul[^<>]*coronavirus(.)in-depth?[^<>]*>(.*?)<\/ul>/g);
+    let split_by_item=split_by_coronavirus[0].match(/<li((?!show-credit).)*?item(.)show?[^<>]*>(.*?)<\/li>/g);
+    let split_by_title3=split_by_item[0].match(/<h(.)[^<>]*title?[^<>]*>(.*?)<\/h\1>/g);
+    let arr3=find(split_by_title3);
+        let arr=arr1.concat(arr2,arr3);
+        res.json({ news: arr });
+      });
+  });
+
+app.get("/getLatestStories", (req, res) => {
+    request(url, function (error, response, html){
+
+    let split=splitting(html);
+    let split_by_latest=split[1].match(/<section[^<>]*latest?[^<>]*>(.*?)<\/section>/g);
+    let split_by_ol=split_by_latest[0].match(/<ol[^<>]*swipe-h?[^<>]*>(.*?)<\/ol>/g);
+    let split_by_stories=split_by_ol[0].match(/<h(.)[^<>]*title?[^<>]*>(.*?)<\/h\1>/g);
+    let array=find(split_by_stories);
+        res.json({ news: array });
+  });
+});
   
 app.get("/getallTimeNews", (req, res) => {
     request(url, function (error, response, html) {
-        result2=[];
+      
         let split_allnews=html.match(/<h(.)(.*[\s+\"\']title[\s+\"\'].*)>(.*?)<\/h\1>/g);
-        // console.log(value);
-        split_allnews.forEach(find2);
-       res.json({ news: result2 });
+        let allnews=find(split_allnews);
+       res.json({ news: allnews });
     });
- });
+});
 
 
 app.listen(port, () => {
